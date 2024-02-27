ad0cb9bcff4186f82a8feef908596e70dfeb9cfa
navidalvand <navidalvand@gmail.com>
2024-02-28 01:38:54 +0330

    server api with no clustering

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/no-cluster", async (req, res) => {
+  let number = 0;
+  for (let ii = 0; ii < 1e9; ii++) {
+    number++;
+  }
+  res.send(`some shi ${number}`);
+});
+
+app.listen(3000, (err) => {
+  if (err) return console.log(err.message);
+  console.log("server is running on port 3000");
+});
